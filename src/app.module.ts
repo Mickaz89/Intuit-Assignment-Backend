9dbd2a85507e9548f3911101e85382b0d5213fff
Mickael Zana <zanamickael@gmail.com>
2024-05-23 14:27:21 +0300

    Initialize Product and Order modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { OrderModule } from './order/order.module';
+import { OrderService } from './order/order.service';
+import { OrderController } from './order/order.controller';
+import { ProductModule } from './product/product.module';
+import { ProductService } from './product/product.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, OrderModule],
+  controllers: [AppController, ProductController, OrderController],
+  providers: [AppService, ProductService, OrderService],
 })
 export class AppModule {}
