35c8b725a7208903686d30e83a25e5bc16552554
Mickael Zana <zanamickael@gmail.com>
2024-05-24 13:00:23 +0300

    Product DTO, service and controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Product, ProductService } from './product.service';
+import { CreateProductDto } from './create-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async getProducts(): Promise<Product[]> {
+    return this.productService.getProducts();
+  }
+
+  @Post('create')
+  async createTask(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    return this.productService.createProduct(createProductDto);
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Type } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ProductModel } from './product.schema';
+import { CreateProductDto } from './create-product.dto';
+
+export interface Product {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+}
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private productModel: Model<ProductModel>,
+  ) {}
+
+  async getProducts(): Promise<Product[]> {
+    return this.productModel.find({});
+  }
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const { name, description, price, image } = createProductDto;
+    const task = new this.productModel({ name, description, price, image });
+    return task.save();
+  }
+}
